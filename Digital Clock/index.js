6e91c2688f72a3383e2582a966b92bfd774007a8
R-E-yawn <aryantyagi1117@gmail.com>
2022-08-15 12:36:09 -0300

    First Commit

---
+++ Digital Clock/index.js
@@ -0,0 +1,35 @@
+const hourEl = document.getElementById("hour");
+const minuteEl = document.getElementById("minutes");
+const secondEl = document.getElementById("seconds");
+const ampmEl = document.getElementById("ampm");
+
+function updateTime(){
+
+    let hour = new Date().getHours();
+    let min = new Date().getMinutes();
+    let sec = new Date().getSeconds();
+    let ampm = "AM";
+
+    if (hour >12){
+        hour -= 12;
+        ampm = "PM";
+    }
+
+    hour = hour < 10 ? "0" + hour : hour;
+    min = min < 10 ? "0" + min : min;
+    sec = sec < 10 ? "0" + sec : sec;
+
+    hourEl.innerText = hour;
+    minuteEl.innerText = min;
+    secondEl.innerText = sec;
+    ampmEl.innerText = ampm;
+
+    setTimeout(() => {
+        updateTime();
+    },1000);
+
+}
+
+updateTime();
+
+
